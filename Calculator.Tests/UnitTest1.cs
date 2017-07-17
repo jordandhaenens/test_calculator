8e12daf06ddabdcbdd5f028d2f740b49126cdecf
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-17 12:13:49 -0500

    finished testing AddNumbers and MultiplyNumbers

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -3,12 +3,32 @@ using Xunit;
 
 namespace Calculator.Tests
 {
-    public class UnitTest1
+    public class CalculatorShould
     {
-        [Fact]
-        public void Test1()
+        private Calculator _calculator;
+        public CalculatorShould()
         {
+            _calculator = new Calculator();
+        }
 
+        [Theory]
+        [InlineData(12, 14)]
+        [InlineData(10, 14)]
+        [InlineData(12, 112)]
+        public void AddTwoNumbers(double value1, double value2)
+        {
+            double sum = _calculator.AddNumbers(value1, value2);
+            Assert.Equal(sum, (value1 + value2));
+        }
+
+        [Theory]
+        [InlineData(12, 14)]
+        [InlineData(10, 14)]
+        [InlineData(12, 112)]
+        public void MultiplyTwoNumbers(double value1, double value2)
+        {
+            double sum = _calculator.MultiplyNumbers(value1, value2);
+            Assert.Equal(sum, (value1 * value2));
         }
     }
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Calculator
 {
+    public class Calculator
+    {
+        private double _sum;
+        public double AddNumbers(double value1, double value2)
+        {
+            _sum = value1 + value2;
+            return _sum;
+        }
+        public double MultiplyNumbers(double value1, double value2)
+        {
+            _sum = value1 * value2;
+            return _sum;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
